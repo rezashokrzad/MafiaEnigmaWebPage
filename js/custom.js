7cc6e10c104dd612cdbb1a007de11018c0d8b288
rezashokrzad@gmail.com <rezashokrzad@gmail.com>
2024-04-16 19:57:57 +0200

    glossary

--- js/custom.js
+++ js/custom.js
@@ -44,3 +44,25 @@
   }
     
 
+// JavaScript function
+function toggleMore(sectionId, buttonTexts = {show: "More", hide: "Less"}, resetTime = 1000) {
+  var moreText = document.getElementById(sectionId);
+  var buttonText = document.querySelector('button[onclick^="toggleMore(\'' + sectionId + '\'"]');
+
+  // Check the current state and toggle accordingly
+  if (moreText.style.display === "none") {
+      moreText.style.display = "block";
+      buttonText.textContent = buttonTexts.hide; // Use "hide" text when showing content
+      buttonText.style.backgroundColor = '#dc3545';
+  } else {
+      moreText.style.display = "none";
+      buttonText.textContent = buttonTexts.show; // Use "show" text when hiding content
+      buttonText.style.backgroundColor = '#dc3545';
+  }
+
+  // Optional: Reset the button color after a specified time
+  setTimeout(function() {
+      buttonText.style.backgroundColor = ''; // Remove the color style
+      buttonText.blur(); // Optionally remove focus from the button
+  }, resetTime); // Default 1000 milliseconds = 1 second
+}
